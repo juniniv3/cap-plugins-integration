9da1bb49daad0d35a632fe30d4c2d9d5e5842191
Yisus Florez <juniniprogramador@outlook.com>
2025-11-24 18:06:10 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cap-plugins-integration',
+  webDir: 'dist'
+};
+
+export default config;
