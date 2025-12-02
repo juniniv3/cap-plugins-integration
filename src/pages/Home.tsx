e6711b017af2224cfd12bc2e323f4df8ca9528bf
Yisus Florez <juniniprogramador@outlook.com>
2025-12-01 23:27:49 -0500

    feature: added action sheet plugin integration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,31 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { ActionSheet, ActionSheetButtonStyle } from '@capacitor/action-sheet';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+
+  const showActionSheet = async () => {
+    const result = await ActionSheet.showActions({
+      title: 'Photo Options',
+      message: 'Select an option to perform',
+      options: [
+        {
+          title: 'example1',
+        },
+        {
+          title: 'example2',
+        },
+        {
+          title: 'example3',
+          style: ActionSheetButtonStyle.Destructive,
+        },
+      ],
+    });
+
+    console.log('Action Sheet result:', result);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +40,7 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <h2 onClick={showActionSheet}>Show Action Sheet</h2>
       </IonContent>
     </IonPage>
   );
